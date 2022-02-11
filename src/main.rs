aabfffde9463b1cf07ecce87b159a14ebee61c98
Mathias <Pfannenkuchen107@gmx.de>
2022-02-11 21:48:42 +0100

    Edited, deleted or changed Files in other directories

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 //! # Xaytex-dashboard
 
+mod appliction;
+
 use actix_web::{App, HttpServer, web};
 use dotenv::dotenv;
 
@@ -8,13 +10,9 @@ use dotenv::dotenv;
 async fn main() -> stq_api::Result<()> {
     dotenv().ok();
 
-    HttpServer::new(|| App::new()
-        .route("/task", web::get().to(handler::task::get_all))
-        .route("/task/{id}", web::get().to(handler::task::get_by_id))
-        .route("/task", web::post().to(handler::task::create_task))
-    )
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+    println!("Starting server...");
+    println!("Starting application...")
+
+
 
 }
